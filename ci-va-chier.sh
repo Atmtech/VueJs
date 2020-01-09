aa52be0d1b0865aa163a807e95372b80444c66a6
Vincent Rioux <vrioux@sqi.gouv.qc.ca>
2020-01-09 14:22:52 -0500

    oh yeah

--- ci-va-chier.sh
+++ ci-va-chier.sh
@@ -1,9 +1,11 @@
-docker system prune
+
 cd /etc/serveur/VueJs
 sudo git pull https://github.com/Atmtech/VueJs.git
 cd /etc/serveur/VueJs/va-chier-api
 docker build -t api.va-chier.com .
 cd /etc/serveur/VueJs/va-chier-site
 docker build -t site.va-chier.com .
+docker system prune
 cd ..
+
 #sudo docker-compose up
