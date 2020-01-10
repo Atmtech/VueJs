b84f66b6698417ac84b0b73142a9bbe251b30868
Vincent Rioux <vrioux@sqi.gouv.qc.ca>
2020-01-10 16:44:09 -0500

    test

--- va-chier-site/src/components/Entete.vue
+++ va-chier-site/src/components/Entete.vue
@@ -1,20 +1,21 @@
 <template>
-     <div
-      class="navbar navbar-dark bg-dark shadow-sm"
-      style="background-color: #4F4943"
-    >
-      <div class="container d-flex justify-content-between">
-        <a href="/" class="navbar-brand d-flex align-items-center">
-          <img src="../assets/logo.png" width="70" height="60" />
-          <h1>VA-CHIER.COM |</h1>
-          <div style="text-transform: uppercase; padding-left:75px;">
-            À tous ceux qui le mérite
-            <br />
-            Allez donc chier !!!
+  <div class="navbar navbar-dark bg-dark shadow-sm">
+    <div class="container">
+      <a href="/" class="navbar-brand d-flex align-items-center">
+        <div class="row">
+          <div class="col text-uppercase">
+            <img src="../assets/logo.png" width="70" height="60" />
           </div>
-        </a>
-      </div>
+          <div class="col text-uppercase">
+            <h1>VA-CHIER.COM</h1>
+            </div>
+           <div class="col text-uppercase">  
+            <h4>À tous ceux qui le mérite ... <br>Allez donc chier bande de merdeux !</h4>
+          </div>
+        </div>
+      </a>
     </div>
+  </div>
 </template>
 <script>
 export default {
