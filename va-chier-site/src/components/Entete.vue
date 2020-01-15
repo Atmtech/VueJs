fe4dfb7c5a305240d2961988354067294f94a978
Vincent Rioux <vrioux@sqi.gouv.qc.ca>
2020-01-15 15:08:55 -0500

    supporter le telephone

--- va-chier-site/src/components/Entete.vue
+++ va-chier-site/src/components/Entete.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="navbar navbar-dark bg-dark shadow-sm">
-    <div class="container">
-      <a href="/" class="navbar-brand d-flex align-items-center">
-        <div class="row">
-          <div class="col text-uppercase">
-            <img src="../assets/logo.png" width="70" height="60" />
-          </div>
-          <div class="col text-uppercase">
-            <h1>VA-CHIER.COM</h1>
-            </div>
-           <div class="col text-uppercase">  
-            <h4>À tous ceux qui le mérite ... <br>Allez donc chier bande de merdeux !</h4>
-          </div>
-        </div>
-      </a>
+    <div class="container text-align-left">
+      <div class="row">
+        <a href="/">
+          <img src="../assets/logo.png" width="70" height="60" />
+        </a>
+        <h2 style="color:white;">VA-CHIER.COM</h2>
+        <h6 class="pl-3" style="color:white;">
+          À tous ceux qui le mérite ... <br />Allez donc chier bande de merdeux
+          !
+        </h6>
+      </div>
     </div>
   </div>
 </template>
